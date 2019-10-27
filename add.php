7dfd21852c2c7cdd979bd50e70355b6292981787
Rezaaisyi <55181853+Rezaaisyi@users.noreply.github.com>
2019-10-27 23:58:46 +0700

    UPDATE ADD.PHP

--- add.php
+++ add.php
@@ -8,24 +8,29 @@
 
  <div class="kotak_add" style="border:0">
 
-    <a href="homepage.php"><img class="logo" src="logo/tu.png" ></a>
+    <a href="index.php"><img class="logo" src="logo/tu.png" ></a>
 
     <form action="add.php" method="post" name="form1">
 
  			<center><label><b>Nama Lengkap</b></label></center>
-            <input type="text" name="Nama_Pengguna" class="form_login" placeholder=". . ."  required="required">
+            <input type="text" name="Nama" class="form_login" placeholder=". . ."  required="required">
             
             <center><label><b>Nama Pengguna</b></label></center>
-            <input type="text" name="Nama" class="form_login" placeholder=". . ."  required="required">
+            <input type="text" name="Nama_Pengguna" class="form_login" placeholder=". . ."  required="required">
             
             <center><label><b>No.HP</b></label></center>
             <input type="text" name="No_hp" class="form_login" placeholder=". . ."  required="required">
 
             <center><label><b>Email</b></label></center>
-            <input type="text" name="Email" class="form_login" placeholder=". . ."  required="required">
+            <input type="email" name="Email" class="form_login" placeholder=". . ."  required="required">
             
             <center><label><b>Kata Sandi</b></label></center>
-			<input type="password" name="password" class="form_login" placeholder=". . ."  required="required">
+            <input type="password" name="password" class="form_login" placeholder=". . ."  required="required">
+
+            <!--<center><label class="custom-file-upload"><input type="file" name="photo" class="inputfile" /><b>Unggah Foto Profil</b> </label></center>-->
+            
+            
+
     <!--<table width="100%" >   
             <tr> 
                 <td>Nama</td>
@@ -61,21 +66,23 @@
 
     <?php
 
-    // Check If form submitted, insert form data into users table.
+
     if(isset($_POST['Submit'])) {
         $nama = $_POST['Nama'];
         $username = $_POST['Nama_Pengguna'];
         $nomor = $_POST['No_hp'];
         $email = $_POST['Email'];
         $password = $_POST['password'];
+        $foto = $_POST['photo'];
 
-        // include database connection file
+        $pw= hash('sha256',$password);
+
+       
         include_once("koneksi.php");
 
-        // Insert user data into table
-        $result = mysqli_query($koneksi, "INSERT INTO akun(Nama,Nama_Pengguna,No_hp,email,password) VALUES('$nama','$username','$nomor','$email','$password')");
+        
+        $result = mysqli_query($koneksi, "INSERT INTO akun(Nama,Nama_Pengguna,No_hp,email,password,photo) VALUES('$nama','$username','$nomor','$email','$pw','$foto')");
 
-        // Show message when user added
         echo "User added successfully. <a href='index.php'>View Users</a>";
         header("location:login.php");}
          
